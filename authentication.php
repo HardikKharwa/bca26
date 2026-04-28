8241ceb7385eee2e6d8c60a790def188a6dabbbd
Hardik <184666881+HardikKharwa@users.noreply.github.com>
2026-04-28 14:28:01 +0530

    update

--- authentication.php
+++ authentication.php
@@ -15,45 +15,45 @@ if (isset($_POST['btnsubmit'])) {
             $_SESSION['error'] = "1";
             $_SESSION['message'] = "All fields are required";
             header("Location:login.php");
-        }
-        else if (strlen($password) < 8) {
+        } else if (strlen($password) < 8) {
             $_SESSION['error'] = "1";
             $_SESSION['message'] = "Password must be at least 8 characters";
             header("Location:login.php");
             echo "Password must be at least 8 characters";
-        }
-        else if (strlen($password) > 8) {
-            $userData = getUserData($conn,"",$username,"");
+        } else if (strlen($password) > 8) {
+            $userData = getUserData($conn, "", $username, "");
             //mysqli_query -> It executes the query from the db and stores the data in obj format
             //$res = mysqli_query($conn,$query);
             // mysqli_num_rows -> IT fetches the count of resultset that is fetched from the query
             //$cnt = mysqli_num_rows($res);
             //if($cnt > 0){
-            if(count($userData) > 0) {
+            if (count($userData) > 0) {
                 //mysqli_fetch_assoc -> Converts data from object format to Array format
                 //$row = mysqli_fetch_assoc($res);
                 $row = $userData[0]['password'];
-                if($row == md5($password)){
-                        $_SESSION['user_id'] = base64_encode($userData[0]['id']);
-                        $_SESSION['username'] = $username;
-                        $_SESSION['isloggedIn'] = "1";
+                if ($row == md5($password)) {
+                    $_SESSION['user_id'] = base64_encode($userData[0]['id']);
+                    $_SESSION['username'] = $username;
+                    $_SESSION['isloggedIn'] = "1";
+                    if (isset($_SESSION['return_url'])) {
+                        $return_url = $_SESSION['return_url'];
+                        unset($_SESSION['return_url']);
+                        header("Location:$return_url");
+                    } else {
+
                         header("Location: dashboard.php");
-                        echo "Login successful";
-                }
-                else{
+                    }
+                } else {
                     $_SESSION['error'] = "1";
                     $_SESSION['message'] = "Login Failed, Try Again!";
                     header("Location:login.php");
                 }
-            }
-            else{
+            } else {
                 $_SESSION['error'] = "1";
                 $_SESSION['message'] = "User not Found, Kindly Register";
                 header("Location:login.php");
             }
-            
-        }
-        else {
+        } else {
             $_SESSION['error'] = "1";
             $_SESSION['message'] = "Some error occured, Try Again!";
             header("Location:login.php");
@@ -61,8 +61,7 @@ if (isset($_POST['btnsubmit'])) {
         }
     }
 
-    /* REGISTER */
-    else if ($_POST['btnsubmit'] == "Register") {
+    /* REGISTER */ else if ($_POST['btnsubmit'] == "Register") {
 
         $FullName = $_POST['FullName'] ?? '';
         $username = $_POST['username'] ?? '';
@@ -72,126 +71,112 @@ if (isset($_POST['btnsubmit'])) {
         $password = $_POST['password'] ?? '';
         $confirm_password = $_POST['confirm_password'] ?? '';
 
-        if (empty($FullName) || empty($username) || empty($phone) || empty($email) || empty($gender) || empty($password) || empty($confirm_password)
+        if (
+            empty($FullName) || empty($username) || empty($phone) || empty($email) || empty($gender) || empty($password) || empty($confirm_password)
         ) {
             echo "All fields are required";
-        }
-        else if ($password != $confirm_password) {
+        } else if ($password != $confirm_password) {
             echo "Passwords do not match";
-        }
-        else if (strlen($password) < 8) {
+        } else if (strlen($password) < 8) {
             echo "Password must be at least 8 characters";
-        }
-        else {
+        } else {
             //echo "Registration successful";
-            $userData = getUserData($conn,"",$username,"");
-            if(count($userData) == 0) {
+            $userData = getUserData($conn, "", $username, "");
+            if (count($userData) == 0) {
                 $enc_password = md5($password);
                 $query = "insert into users(username,password) VALUES('$username','$enc_password')";
-                if(mysqli_query($conn,$query)){
+                if (mysqli_query($conn, $query)) {
                     echo "Registration successful";
-                }
-                else{
+                } else {
                     echo "Failed to register user";
                 }
-            }
-            else{
+            } else {
                 echo "User Already Registered, Kindly Login";
             }
         }
     }
-    /* User Data Edit */
-    else if($_POST['btnsubmit'] == "Update User"){
+    /* User Data Edit */ else if ($_POST['btnsubmit'] == "Update User") {
         $uid = base64_decode(base64_decode($_POST['uid']));
         $username = $_POST['username'];
         $phone = $_POST['phone'];
         $email = $_POST['email'];
 
-        if((strlen($uid) > 0 && $uid != "") && (strlen($username) > 0 && $username != "")){
-            $checkUserData = getUserData($conn,$uid,"","");
-            if(count($checkUserData) > 0){
+        if ((strlen($uid) > 0 && $uid != "") && (strlen($username) > 0 && $username != "")) {
+            $checkUserData = getUserData($conn, $uid, "", "");
+            if (count($checkUserData) > 0) {
                 $updateUserQuery = "update users set username= '$username',contact='$phone',email='$email' where id = $uid";
-                if(mysqli_query($conn,$updateUserQuery)){
-                    ?>
+                if (mysqli_query($conn, $updateUserQuery)) {
+?>
                     <script>
                         alert("User Data Updated!");
-                        window.location.href='dashboard.php';
+                        window.location.href = 'dashboard.php';
                     </script>
-                    <?php
-                }
-                else{
-                    ?>
+                <?php
+                } else {
+                ?>
                     <script>
                         alert("Failed to Update Data of User!");
-                        window.location.href='user_edit.php?id=<?php echo $uid ?>';
+                        window.location.href = 'user_edit.php?id=<?php echo $uid ?>';
                     </script>
-                    <?php
+<?php
                 }
-            }
-            else{
+            } else {
                 header("Location:dashboard.php");
             }
         }
     }
-    /* Upload User Image */
-    else if($_POST['btnsubmit'] == "Upload User Image"){
+    /* Upload User Image */ else if ($_POST['btnsubmit'] == "Upload User Image") {
         $user_id = base64_decode($_SESSION['user_id']);
-        $checkUser = getUserData($conn,$user_id,"","");
+        $checkUser = getUserData($conn, $user_id, "", "");
         $file_path = "images_user/"; //Folder where the image will be saved
-        if(count($checkUser) > 0){
-            if(is_uploaded_file($_FILES['user_image']['tmp_name'])){
+        if (count($checkUser) > 0) {
+            if (is_uploaded_file($_FILES['user_image']['tmp_name'])) {
                 $filename = $_FILES['user_image']['name'];
                 $extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
                 $allowed_extensions = ['jpg', 'jpeg', 'png'];
                 if (in_array($extension, $allowed_extensions)) {
-                    if($_FILES["user_image"]["size"] < 5000000){
+                    if ($_FILES["user_image"]["size"] < 5000000) {
                         //echo "File size is valid";
                         $save_file = $file_path . $filename;
-                        if(move_uploaded_file($_FILES['user_image']['tmp_name'],$save_file)){
+                        if (move_uploaded_file($_FILES['user_image']['tmp_name'], $save_file)) {
                             //echo "File saved to directory";
                             $updateImageQuery = "update users set image_path='$filename' where id = $user_id";
-                            if(mysqli_query($conn,$updateImageQuery)){
+                            if (mysqli_query($conn, $updateImageQuery)) {
                                 //echo "User Image Saved Successfully";
                                 $_SESSION['error'] = "1";
                                 $_SESSION['message'] = "User Image Saved Successfully";
                                 header("Location:profile.php");
-                            }
-                            else{
+                            } else {
                                 //echo "Failed to save user image, Try Again!";
                                 $_SESSION['error'] = "0";
                                 $_SESSION['message'] = "Failed to save user image, Try Again!";
                                 header("Location:profile.php");
                             }
-                        }
-                        else{
+                        } else {
                             //echo "Failed to upload image";
                             $_SESSION['error'] = "0";
                             $_SESSION['message'] = "Failed to upload image";
                             header("Location:profile.php");
                         }
-                    }
-                    else{
+                    } else {
                         //echo "File size is greater than 5MB";
-                            $_SESSION['error'] = "0";
-                            $_SESSION['message'] = "File size is greater than 5MB";
-                            header("Location:profile.php");                        
+                        $_SESSION['error'] = "0";
+                        $_SESSION['message'] = "File size is greater than 5MB";
+                        header("Location:profile.php");
                     }
-                }
-                else{
+                } else {
                     //echo "Please select file of valid extension";
-                            $_SESSION['error'] = "0";
-                            $_SESSION['message'] = "Please select file of valid extension";
-                            header("Location:profile.php");                       
+                    $_SESSION['error'] = "0";
+                    $_SESSION['message'] = "Please select file of valid extension";
+                    header("Location:profile.php");
                 }
-            }
-            else{
+            } else {
                 echo "Please select file";
                 $_SESSION['error'] = "0";
                 $_SESSION['message'] = "Please select file";
                 header("Location:profile.php");
             }
-        }
-        else{
+        } else {
             echo "User not found";
             $_SESSION['error'] = "0";
             $_SESSION['message'] = "User not found";
--- profile.php
+++ profile.php
@@ -3,58 +3,63 @@ include("database.php");
 include("includes/functions.inc.php");
 session_start();
 $user_id = base64_decode($_SESSION['user_id']);
+if (!(isset($_SESSION['user_id']))) {
+    // header("Location:logout.php");
+    $_SESSION['return_url'] = "profile.php";
+    header('location:login.php');
+}
 ?>
 <html>
-    <head>
-        <title>My Profile</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-    </head>
-    <body class="container">
-        <h3>My Profile</h3>
-        <?php
-            if(isset($_SESSION['error']) && isset($_SESSION['message'])){
-                if($_SESSION['error'] != "" && $_SESSION['message'] != ""){
-                    $msg = $_SESSION['message'];
-                    if($_SESSION['error'] == "1"){
-                        $status = "success";
-                    }
-                    else if($_SESSION['error'] == "0"){
-                        $status = "danger";
-                    }
-                    echo "<div class='btn btn-".$status."'>
-                        ".$msg."
+
+<head>
+    <title>My Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+</head>
+
+<body class="container">
+    <h3>My Profile</h3>
+    <?php
+    if (isset($_SESSION['error']) && isset($_SESSION['message'])) {
+        if ($_SESSION['error'] != "" && $_SESSION['message'] != "") {
+            $msg = $_SESSION['message'];
+            if ($_SESSION['error'] == "1") {
+                $status = "success";
+            } else if ($_SESSION['error'] == "0") {
+                $status = "danger";
+            }
+            echo "<div class='btn btn-" . $status . "'>
+                        " . $msg . "
                     </div>";
-                    unset($_SESSION['error']);
-                    unset($_SESSION['message']);
-                }
-            }
-        ?>
-        <form action="authentication.php" method="POST" enctype="multipart/form-data">
-            Upload Your Photo:
-            <input class="form-control" type="file" name="user_image" 
+            unset($_SESSION['error']);
+            unset($_SESSION['message']);
+        }
+    }
+    ?>
+    <form action="authentication.php" method="POST" enctype="multipart/form-data">
+        Upload Your Photo:
+        <input class="form-control" type="file" name="user_image"
             accept=".jpg,.jpeg,.png" required />
-            <br>
-            <input type="submit" 
-            class="form-control btn btn-primary" 
+        <br>
+        <input type="submit"
+            class="form-control btn btn-primary"
             name="btnsubmit" value="Upload User Image" />
-        </form>
-        <div class="container">
-            <strong>Users Profile Image</strong><br/>
+    </form>
+    <div class="container">
+        <strong>Users Profile Image</strong><br />
         <?php
-            $userData = getUserData($conn,$user_id,"","");
-            if($userData[0]['image_path'] != ""){
+        $userData = getUserData($conn, $user_id, "", "");
+        if ($userData[0]['image_path'] != "") {
         ?>
-                <img src="images_user/<?php echo $userData[0]['image_path']; ?>" 
+            <img src="images_user/<?php echo $userData[0]['image_path']; ?>"
                 alt="<?php echo $userData[0]['username'] . 'Profile Image' ?>"
-                style="height:50%;border-radius:10px;"
-                />
+                style="height:50%;border-radius:10px;" />
         <?php
-            }
-            else{
-                echo "No Image Found for User";
-            }
+        } else {
+            echo "No Image Found for User";
+        }
         ?>
-        </div>
-        </body>
-    </body>
+    </div>
+</body>
+</body>
+
 </html>
